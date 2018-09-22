484b70a95fec9c07dddcdab4bed0991f0f11c79f
shivamengineersworkbook <shivamkalra2017@gmail.com>
2018-09-23 02:36:02 +0530

    completed 2 parts

---
+++ learning/src/components/header.js
@@ -0,0 +1,3 @@
+import React from "react"
+
+export default props => <h1>This is a {props.HeaderText}.</h1>;
---
+++ learning/src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "../components/header.js"
+
+
+export default () => (
+    <div style={{ color: `teal` }}>
+        <Header HeaderText = "About Page"></Header>
+        <h1>About Gatsby</h1>
+        <p>Such wow. Very React.</p>
+    </div>
+)
