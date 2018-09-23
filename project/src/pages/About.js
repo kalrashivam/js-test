3893ed720ecbc117b79a8c20f9dab97951443c39
shivamengineersworkbook <shivamkalra2017@gmail.com>
2018-09-23 13:51:18 +0530

    started image gallery project

---
+++ project/src/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout.js"
+
+export default () => (
+    <Layout>
+        <div>
+            This is the about page
+        </div>
+    </Layout>
+)
